f2bf5781f2d811487fd90709e922dd12425fd362
Lindsay Foley <lindsay.foley@outlook.com>
2025-02-23 17:29:27 +0000

    About WIP

---
+++ src/components/twoColumnGrid/twoColumnGrid.tsx
@@ -0,0 +1,5 @@
+const TwoColumnGrid = () => {
+  return <>hi</>;
+};
+
+export default TwoColumnGrid;
---
+++ src/constants/about/content.js
@@ -0,0 +1,12 @@
+export const ABOUT_CONTENT = {
+  meta: {
+    title: "About Portfoleyo | Web Development & Digital Solutions",
+    description:
+      "Discover Portfoleyo's expertise in web development, SEO, and scalable digital solutions. Over a decade of experience helping brands build high performing sites",
+  },
+  sectionTitle: {
+    title: "About Portfoleyo",
+    subtitle: "A decade of digital expertise",
+    isH1Element: true,
+  },
+};
