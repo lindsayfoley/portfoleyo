8fb9818b8e60ba179433c2c465935c200b56d8d8
Lindsay Foley <lindsay.foley@outlook.com>
2025-02-25 18:55:05 +0000

    adjusting layout

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -7,25 +7,27 @@ import { NAV_CONTENT } from "../../constants/nav/nav";
 const Nav = () => {
   return (
     <nav className={styles.container}>
-      <div className={`${styles.pages} ${styles.links}`}>
-        <ul className={styles.linksList}>
-          {NAV_CONTENT.headerNavContent.map(({ href, cta }) => (
-            <li key={cta}>
-              <Link href={href}>{cta}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={`${styles.icons} ${styles.links}`}>
-        <ul className={styles.linksList}>
-          {NAV_CONTENT.icons.map(({ title, href, target, icon }) => (
-            <li key={title}>
-              <a title={title} href={href} target={target}>
-                <FontAwesomeIcon icon={icon as IconDefinition} size="lg" />
-              </a>
-            </li>
-          ))}
-        </ul>
+      <div className={styles.contentWrapper}>
+        <div className={`${styles.pages} ${styles.links}`}>
+          <ul className={styles.linksList}>
+            {NAV_CONTENT.headerNavContent.map(({ href, cta }) => (
+              <li key={cta}>
+                <Link href={href}>{cta}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={`${styles.icons} ${styles.links}`}>
+          <ul className={styles.linksList}>
+            {NAV_CONTENT.icons.map(({ title, href, target, icon }) => (
+              <li key={title}>
+                <a title={title} href={href} target={target}>
+                  <FontAwesomeIcon icon={icon as IconDefinition} size="lg" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
