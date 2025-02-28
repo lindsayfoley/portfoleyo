ee5e590c504f6b0b3282cf540fa8653fd7360b2f
Lindsay Foley <lindsay.foley@outlook.com>
2025-02-28 14:14:21 +0000

    fixing mobile and tablet styles

--- src/components/featureTiles/featureTiles.tsx
+++ src/components/featureTiles/featureTiles.tsx
@@ -39,31 +39,47 @@ const FeatureTiles = ({
   cta,
   theme = colourTheme.default,
 }: FeatureTilesProps) => {
+  const tilesMarkup = services.map(
+    ({ image, Icon, title, description, href }) => {
+      return href ? (
+        <article className={styles.service} key={title}>
+          <a href={href}>
+            {image && (
+              <img className={styles.image} src={image.src} alt={image.alt} />
+            )}
+            {Icon && (
+              <Icon fill={getIconFillValueFromTheme(theme)} width="100px" />
+            )}
+            <div className={styles.copy}>
+              <h4 className={styles.title}>{title}</h4>
+              <p className={styles.description}>{description}</p>
+              {cta && <h5 className={styles.cta}>{cta}</h5>}
+            </div>
+          </a>
+        </article>
+      ) : (
+        <article className={styles.service} key={title}>
+          {image && (
+            <img className={styles.image} src={image.src} alt={image.alt} />
+          )}
+          {Icon && (
+            <Icon fill={getIconFillValueFromTheme(theme)} width="100px" />
+          )}
+          <div className={styles.copy}>
+            <h4 className={styles.title}>{title}</h4>
+            <p className={styles.description}>{description}</p>
+            {cta && <h5 className={styles.cta}>{cta}</h5>}
+          </div>
+        </article>
+      );
+    }
+  );
+
   return (
     <section className={theme ? `${theme}Theme` : ""}>
       <div className={styles.container}>
         <SectionTitle title={title} subtitle={subtitle} />
-        <div className={styles.serviceWrapper}>
-          {services.map(({ image, Icon, title, description, href }) => (
-            <article className={styles.service} key={title}>
-              {image && (
-                <img className={styles.image} src={image.src} alt={image.alt} />
-              )}
-              {Icon && (
-                <Icon fill={getIconFillValueFromTheme(theme)} width="100px" />
-              )}
-              <div className={styles.copy}>
-                <h4 className={styles.title}>{title}</h4>
-                <p className={styles.description}>{description}</p>
-                {href && cta && (
-                  <a className={styles.link} href={href}>
-                    {cta}
-                  </a>
-                )}
-              </div>
-            </article>
-          ))}
-        </div>
+        <div className={styles.serviceWrapper}>{tilesMarkup}</div>
       </div>
     </section>
   );
