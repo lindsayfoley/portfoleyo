18ba447a1f08bcf5babdc9d3e8bdb26e04ffc035
Lindsay Foley <lindsay.foley@outlook.com>
2025-03-05 18:25:47 +0000

    adding content to Portfolio page

--- src/constants/porfolio/content.ts
+++ src/constants/porfolio/content.ts
@@ -1,4 +1,9 @@
 import { colourTheme } from "@portfoleyo/shared/common";
+import {
+  faHammer,
+  faCalendarAlt,
+  faBriefcase,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const PORTFOLIO_CONTENT = {
   meta: {
@@ -19,5 +24,123 @@ export const PORTFOLIO_CONTENT = {
     isTitleFirst: true,
     isH1Element: true,
     theme: colourTheme.highlight,
+  },
+  checkmarkSectionTitle: "Key Updates:",
+  theFabledThread: {
+    twoColumn: {
+      title: "The Fabled Thread",
+      titleHref: "https://thefabledthread.com/",
+      theme: colourTheme.highlight,
+      image: {
+        src: "https://place-hold.it/400x300/#ffff",
+        alt: "",
+      },
+    },
+    list: [
+      {
+        text: "From August 2024 - present",
+        icon: faCalendarAlt,
+      },
+      {
+        text: "Remix, React, TypeScript, Shopify (Hydrogen), Sanity CMS",
+        icon: faHammer,
+      },
+      {
+        text: "I support The Fabled Thread on an ad-hoc basis, overseeing full-stack development across their Remix codebase, Shopify Oxygen, and Sanity CMS.",
+        icon: faBriefcase,
+      },
+    ],
+    workCompletedList: [
+      "Performance: optimised image loading by refining Sanity parameters & assets",
+      "SEO: added hreflang tags to boost international visibility & rankings",
+      "CMS: built Sanity models, enabling editors to update content without dev input",
+    ],
+  },
+  theMews: {
+    twoColumn: {
+      title: "The Mews Fabrics & Furnishings",
+      titleHref: "https://themewsfurnishings.com/",
+      theme: colourTheme.highlight,
+      image: {
+        src: "https://place-hold.it/400x300/#ffff",
+        alt: "",
+      },
+    },
+    list: [
+      {
+        text: "January 2025",
+        icon: faCalendarAlt,
+      },
+      {
+        text: "Shopify site",
+        icon: faHammer,
+      },
+      {
+        text: "I support The Mews Fabrics & Furnishings on an ad-hoc basis, handling site optimisations, resolving technical issues, and refining functionality.",
+        icon: faBriefcase,
+      },
+    ],
+    workCompletedList: [
+      "SEO: audited and optimised meta titles and descriptions across the site",
+      "User Experience: fixed email signup discount code and integrated Klaviyo",
+    ],
+  },
+  lesterre: {
+    twoColumn: {
+      title: "L’Esterre Chocolate",
+      titleHref: "https://lesterrechocolate.com/",
+      theme: colourTheme.highlight,
+      image: {
+        src: "https://place-hold.it/400x300/#ffff",
+        alt: "",
+      },
+    },
+    list: [
+      {
+        text: "February 2025",
+        icon: faCalendarAlt,
+      },
+      {
+        text: "Shopify site",
+        icon: faHammer,
+      },
+      {
+        text: "I worked with L’Esterre Chocolate to enhance their homepage and boost credibility by implementing a press showcase carousel, alongside conducting a detailed site audit to identify improvements in performance, SEO, and UX.",
+        icon: faBriefcase,
+      },
+    ],
+    workCompletedList: [
+      "Press Features: added “As Seen In” carousel to homepage, highlighting coverage",
+    ],
+  },
+  charlotteTilbury: {
+    twoColumn: {
+      title: "Charlotte Tilbury",
+      titleHref: "https://www.charlottetilbury.com/",
+      theme: colourTheme.highlight,
+      image: {
+        src: "https://place-hold.it/400x300/#ffff",
+        alt: "",
+      },
+    },
+    list: [
+      {
+        text: "May 2023 - December 2024",
+        icon: faCalendarAlt,
+      },
+      {
+        text: "React, TypeScript, Next.js, Storybook, Contentful",
+        icon: faHammer,
+      },
+      {
+        text: "I optimised Charlotte Tilbury’s SEO, improved platform performance, and played a key role in shaping a new Design System to ensure sitewide consistency.",
+        icon: faBriefcase,
+      },
+    ],
+    workCompletedList: [
+      "Design System: developed reusable components to standardise UI sitewide",
+      "SEO: implemented schema markup for products, reviews, and blog posts",
+      "Performance: upgraded Storybook to improve maintainability and security",
+    ],
   },
 };
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
-import { PORTFOLIO_CONTENT } from "@portfoleyo/constants/porfolio/content";
 import Banner from "@portfoleyo/components/banner/banner";
+import { PORTFOLIO_CONTENT } from "@portfoleyo/constants/porfolio/content";
+import TwoColumnLayout from "@portfoleyo/components/twoColumnLayout/twoColumnLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/portfolio.module.css";
 
 export default function Portfolio() {
   return (
@@ -11,14 +15,111 @@ export default function Portfolio() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <Banner
-          title={PORTFOLIO_CONTENT.banner.title}
-          subtitle={PORTFOLIO_CONTENT.banner.subtitle}
-          isTitleFirst={PORTFOLIO_CONTENT.banner.isTitleFirst}
-          isH1Element={PORTFOLIO_CONTENT.banner.isH1Element}
-          description={PORTFOLIO_CONTENT.banner.description}
-          theme={PORTFOLIO_CONTENT.banner.theme}
-        />
+        <Banner {...PORTFOLIO_CONTENT.banner} isIndependantSection={false} />
+        <TwoColumnLayout
+          {...PORTFOLIO_CONTENT.theFabledThread.twoColumn}
+          isIndependantSection={false}
+        >
+          <ul className={`list ${styles.list}`}>
+            {PORTFOLIO_CONTENT.theFabledThread.list.map(({ text, icon }) => (
+              <li key={text}>
+                <h5 className="heading">
+                  <FontAwesomeIcon className="icon" icon={icon} />
+                </h5>
+                {text}
+              </li>
+            ))}
+            <li>
+              <h5 className="heading">
+                {PORTFOLIO_CONTENT.checkmarkSectionTitle}
+              </h5>
+              {PORTFOLIO_CONTENT.theFabledThread.workCompletedList.map(
+                (item) => (
+                  <li key={item}>
+                    <FontAwesomeIcon className="icon" icon={faCheck} /> {item}
+                  </li>
+                )
+              )}
+            </li>
+          </ul>
+        </TwoColumnLayout>
+        <TwoColumnLayout
+          {...PORTFOLIO_CONTENT.theMews.twoColumn}
+          isIndependantSection={false}
+        >
+          <ul className={`list ${styles.list}`}>
+            {PORTFOLIO_CONTENT.theMews.list.map(({ text, icon }) => (
+              <li key={text}>
+                <h5 className="heading">
+                  <FontAwesomeIcon className="icon" icon={icon} />
+                </h5>
+                {text}
+              </li>
+            ))}
+            <li>
+              <h5 className="heading">
+                {PORTFOLIO_CONTENT.checkmarkSectionTitle}
+              </h5>
+              {PORTFOLIO_CONTENT.theMews.workCompletedList.map((item) => (
+                <li key={item}>
+                  <FontAwesomeIcon className="icon" icon={faCheck} /> {item}
+                </li>
+              ))}
+            </li>
+          </ul>
+        </TwoColumnLayout>
+        <TwoColumnLayout
+          {...PORTFOLIO_CONTENT.lesterre.twoColumn}
+          isIndependantSection={false}
+        >
+          <ul className={`list ${styles.list}`}>
+            {PORTFOLIO_CONTENT.lesterre.list.map(({ text, icon }) => (
+              <li key={text}>
+                <h5 className="heading">
+                  <FontAwesomeIcon className="icon" icon={icon} />
+                </h5>
+                {text}
+              </li>
+            ))}
+            <li>
+              <h5 className="heading">
+                {PORTFOLIO_CONTENT.checkmarkSectionTitle}
+              </h5>
+              {PORTFOLIO_CONTENT.lesterre.workCompletedList.map((item) => (
+                <li key={item}>
+                  <FontAwesomeIcon className="icon" icon={faCheck} /> {item}
+                </li>
+              ))}
+            </li>
+          </ul>
+        </TwoColumnLayout>
+        <TwoColumnLayout
+          {...PORTFOLIO_CONTENT.charlotteTilbury.twoColumn}
+          isIndependantSection={false}
+        >
+          <ul className={`list ${styles.list}`}>
+            {PORTFOLIO_CONTENT.charlotteTilbury.list.map(({ text, icon }) => (
+              <li key={text}>
+                <h5 className="heading">
+                  <FontAwesomeIcon className="icon" icon={icon} />
+                </h5>
+                {text}
+              </li>
+            ))}
+            <li>
+              <h5 className="heading">
+                {PORTFOLIO_CONTENT.checkmarkSectionTitle}
+              </h5>
+              {PORTFOLIO_CONTENT.charlotteTilbury.workCompletedList.map(
+                (item) => (
+                  <li key={item}>
+                    <FontAwesomeIcon className="icon" icon={faCheck} /> {item}
+                  </li>
+                )
+              )}
+            </li>
+          </ul>
+        </TwoColumnLayout>
       </main>
     </>
   );
