e30c2599e3d04ec1cdb46f7251e4c04865f8af25
Lindsay Foley <lindsay.foley@outlook.com>
2025-03-06 16:10:06 +0000

    Adding GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,24 @@ import Nav from "@portfoleyo/components/nav/nav";
 import { NAV_CONTENT } from "@portfoleyo/constants/nav/nav";
 import Footer from "@portfoleyo/components/footer/footer";
 import "@portfoleyo/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-K5J1VV23Z9"
+      ></Script>
+      <Script
+        id="GA"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K5J1VV23Z9');`,
+        }}
+      />
       <Nav links={NAV_CONTENT.headerNavContent} />
       <Component {...pageProps} />
       <Footer />
