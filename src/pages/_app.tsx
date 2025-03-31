3b7cf62b66544e9809616d84728aa029c0177f8e
Lindsay Foley <lindsay.foley@outlook.com>
2025-03-31 16:29:10 +0100

    adding canonical tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,12 @@ import Nav from "@portfoleyo/components/nav/nav";
 import { NAV_CONTENT } from "@portfoleyo/constants/nav/nav";
 import Footer from "@portfoleyo/components/footer/footer";
 import "@portfoleyo/styles/globals.css";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <>
       <Script
@@ -21,6 +25,12 @@ export default function App({ Component, pageProps }: AppProps) {
               gtag('config', 'G-K5J1VV23Z9');`,
         }}
       />
+      <Head>
+        <link
+          rel="canonical"
+          href={`https://theportfoleyo.com${router?.asPath?.split("?")[0]}`}
+        />
+      </Head>
       <Nav links={NAV_CONTENT.links} />
       <Component {...pageProps} />
       <Footer />
