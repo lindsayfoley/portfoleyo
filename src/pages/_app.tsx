93d89511f460caa58425697e4ce24aa300479665
Lindsay Foley <lindsay.foley@outlook.com>
2025-04-25 15:29:10 +0100

    adding global use animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,10 @@ import type { AppProps } from "next/app";
 import Nav from "@portfoleyo/components/nav/nav";
 import { NAV_CONTENT } from "@portfoleyo/constants/nav/nav";
 import Footer from "@portfoleyo/components/footer/footer";
-import "@portfoleyo/styles/globals.css";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import "@portfoleyo/styles/globals.css";
+import "@portfoleyo/styles/animations.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
