f5532476b45df2f033be4d98767dd8a32820359e
Lindsay Foley <lindsay.foley@outlook.com>
2025-04-25 16:36:57 +0100

    abstracting function

--- src/shared/common.tsx
+++ src/shared/common.tsx
@@ -5,3 +5,15 @@ export const colourTheme = {
 } as const;
 
 export type Theme = (typeof colourTheme)[keyof typeof colourTheme];
+
+export const getIconFillValueFromTheme = (theme: Theme) => {
+  if (theme === colourTheme.muted) {
+    return "#ffffff";
+  }
+
+  if (theme === colourTheme.highlight) {
+    return "#faf1ec";
+  }
+
+  return "#f8f7f3";
+};
