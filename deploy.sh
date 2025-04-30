7b6d1274588564105c593f5cb2c373ba51055c36
Lindsay Foley <lindsay.foley@outlook.com>
2025-04-30 17:15:55 +0100

    correcting scipts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-npm i && NEXT_DISABLE_ESLINT=1 NEXT_DISABLE_TYPECHECK=1 npm run build
+export NODE_OPTIONS="--max-old-space-size=8192"
+export NEXT_DISABLE_ESLINT=1
+export NEXT_DISABLE_TYPECHECK=1
+
+npm i && npm run build
 cp -r out/* /home/lindsay/public/theportfoleyo.com/public
 ls /home
 
