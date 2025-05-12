4f6e535c0a12ff85bbe917bab47977ac54d26aa0
Lindsay Foley <lindsay.foley@outlook.com>
2025-05-12 11:42:33 +0100

    adding mime type support for next gen image formats

--- src/components/lazyPicture/lazyPicture.tsx
+++ src/components/lazyPicture/lazyPicture.tsx
@@ -16,19 +16,19 @@ const LazyPicture = ({
   classname,
 }: LazyPictureProps) => {
   const { src, alt } = image;
-  //   const defaultNextGenImageFormat = ["avif", "webp"];
-  //   const imageSrcWithoutFormat = src.slice(0, src.indexOf("."));
+  const supportedNextGenImageFormat = ["avif", "webp"];
+  const imageSrcWithoutFormat = src.slice(0, src.indexOf("."));
 
   return (
     <picture>
       {mediaCondition && <source {...mediaCondition} />}
-      {/* {defaultNextGenImageFormat.map((format) => (
+      {supportedNextGenImageFormat.map((format) => (
         <source
           key={format}
           srcSet={`${imageSrcWithoutFormat}.${format}`}
           type={`image/${format}`}
         />
-      ))} */}
+      ))}
       <img className={classname} src={src} alt={alt} loading="lazy" />
     </picture>
   );
