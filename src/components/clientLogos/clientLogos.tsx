982114c825a493fc1fedde09fedf112c1bec29b6
Lindsay Foley <lindsay.foley@outlook.com>
2025-07-14 00:06:34 +0100

    adding new component for Client Logos

---
+++ src/components/clientLogos/clientLogos.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import styles from "./clientLogos.module.css";
+
+const title = " Brands I've worked with:";
+
+const ClientLogos = () => {
+  return (
+    <section className={styles.container}>
+      <span className={`${styles.title} ${styles.hideDesktop}`}>{title}</span>
+      <ul className={styles.list}>
+        <li className={`${styles.title} ${styles.hideMobile}`}>{title}</li>
+        <li>
+          <Link href="/portfolio/#charlotte-tilbury">
+            <img
+              className={styles.ct}
+              src="/charlotte-tilbury-logo.png"
+              alt="Charlotte Tilbury logo"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link href="/portfolio/#dcypher">
+            <img
+              className={styles.dcypher}
+              src="/dcypher-logo.png"
+              alt="DCYPHER logo"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link href="/portfolio/#the-fabled-thread">
+            <img
+              className={styles.tft}
+              src="/the-fabled-thread-logo.avif"
+              alt="The Fabled Thread logo"
+            />
+          </Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default ClientLogos;
