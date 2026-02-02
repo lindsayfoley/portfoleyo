97491c0f58e121eca99d0195a05f3ad72cc76b16
Lindsay Foley <lindsay.foley@outlook.com>
2026-02-02 15:45:39 +0000

    removing projects link from footer

--- src/constants/footer/content.ts
+++ src/constants/footer/content.ts
@@ -5,6 +5,9 @@ export const FOOTER_CONTENT = {
     description:
       "At The Portfoleyo, I help brands bring their ideas to life with modern, accessible, and high-performance web development. Whether it’s improving an existing site or creating something new from scratch, I focus on clean, efficient code and thoughtful design—ensuring everything runs smoothly and feels great to use.",
   },
+  browse: {
+    title: "Browse",
+  },
   projects: {
     title: "Projects",
     link: "/projects",
