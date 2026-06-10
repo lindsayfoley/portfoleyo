91d346e5b580f20b403cb8b568188f65a340b6d9
Lindsay Foley <lindsay.foley@outlook.com>
2026-06-10 17:00:55 +0100

    fixing error and removing uneeded optional chaining

--- src/pages/projects/hydrogen-merchant-center-domain-setup.tsx
+++ src/pages/projects/hydrogen-merchant-center-domain-setup.tsx
@@ -11,20 +11,20 @@ export default function MerchantCenterDomains() {
   return (
     <>
       <Head>
-        <title>{project?.meta?.title}</title>
-        <meta name="description" content={project?.meta?.description} />
+        <title>{project.meta.title}</title>
+        <meta name="description" content={project.meta.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="og:image"
-          content={`${DOMAIN}${project?.twoColumn?.image?.src}`}
+          content={`${DOMAIN}${project.twoColumn.image.src}`}
         />
       </Head>
       <main className={`${styles.main} highlightTheme`}>
         <Banner
-          title={project?.h1}
-          subtitle={project?.subTitle}
+          title={project.h1}
+          subtitle={project.subTitle}
           isH1Element
-          image={project?.twoColumn?.image}
+          image={project.twoColumn.image}
         />
         <article className={styles.article}>
           <div dangerouslySetInnerHTML={{ __html: allContent }} />
